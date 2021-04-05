46c2be83888f8bac6f55e85242562bb9d3494996
preethi <preethimandipalli99@gmail.com>
2021-04-05 23:35:30 +0530

    Added functionality to check Probablities are equal or not

--- src/main/java/utility/Coin.java
+++ src/main/java/utility/Coin.java
@@ -1,4 +1,19 @@
 package utility;
 
 public class Coin {
+    double totalPossibleOutcomes;
+    int coinsCount;
+
+    public Coin(int coinsCount) {
+        this.coinsCount=coinsCount;
+    }
+
+    public boolean checkEqualityOfProbability() {
+        totalPossibleOutcomes= Math.pow(2,coinsCount);
+        double headProbability = (1/totalPossibleOutcomes);
+        double tailProbability= 1/(totalPossibleOutcomes);
+        return headProbability==tailProbability;
+
+    }
+
 }
--- src/test/java/utility/TestCoin.java
+++ src/test/java/utility/TestCoin.java
@@ -1,5 +1,19 @@
 package utility;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCoin {
+
+    @Test
+    void TestToCheckIfProbabilityOfHeadIsEqualToThatOfTail(){
+        int count=1;
+        Coin coin=new Coin(count);
+        boolean expected=true;
+
+        boolean actual=coin.checkEqualityOfProbability();
+
+        assertEquals(expected,actual);
+
+    }
 
 }
